4c7cfea4d47c94fe35fc8573ad67adabcb56b301
Nauzet Hernandez <nauzethc@gmail.com>
2026-01-11 15:06:04 +0000

    sddm theme installer

---
+++ install/themes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CURRENT=$(pwd)
+cd /tmp
+
+# Install Catppuccin theme for SDDM
+wget https://github.com/catppuccin/sddm/releases/download/v1.1.2/catppuccin-macchiato-mauve-sddm.zip
+
+if [ ! -f catppuccin-macchiato-mauve-sddm.zip ]; then
+  7z x catppuccin-macchiato-mauve-sddm.zip
+  sudo cp -r catppuccin-macchiato-mauve /usr/share/sddm/themes/
+  sudo tee /etc/sddm.conf <<'EOF'
+[Theme]
+Current=catppuccin-macchiato-mauve
+EOF
+fi
+
