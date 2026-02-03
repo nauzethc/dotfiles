8fb14c47e1ac82bc77108cfc2ac1cbc86773b394
Nauzet Hernandez <nauzethc@gmail.com>
2026-02-03 12:13:44 +0000

    new xbox driver added

--- install/hardware/xbox.sh
+++ install/hardware/xbox.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Install new Xbox driver compatible with Xbox Elite Controller 2
+yay -Sy --needed --noconfirm xpadneo-dkms
+
 # Give this user privileged input access for dictation tools + xbox controllers to work
 sudo usermod -aG input ${USER}
