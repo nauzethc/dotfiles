16456bce2a4a0a23ff53103fb18032c393c9169d
Nauzet Hernandez <nauzethc@gmail.com>
2026-02-03 12:13:11 +0000

    SteamOS launchers and configuration files to enable HDR

--- install/desktop/steamos.sh
+++ install/desktop/steamos.sh
@@ -7,7 +7,32 @@ sudo pacman -Sy --needed --noconfirm \
   sddm \
   steam
 
-# Install ProtonPlus to enable Proton with HDR support
-# flatpak install --noninteractive com.vysp3r.ProtonPlus
+# Install Wayland's HDR compatible Proton and Vulkan layer
+yay -S --needed --noconfirm \
+  gamescope-session-git \
+  moondeckbuddy-appimage \
+  proton-ge-custom-bin \
+  sunshine \
+  vk-hdr-layer-kwin6-git
+
+sudo mkdir -p /usr/share/wayland-sessions
+sudo mkdir -p /usr/share/gamescope-session-plus/sessions.d
+
+# Add Steam session to Gamescope
+sudo tee /usr/share/gamescope-session-plus/sessions.d/steam <<'EOF'
+CLIENTCMD="steam -steamdeck -steamos3 -pipewire-dmabuf"
+EOF
+
+# Add SteamOS with GameScope session on login managers
+echo "Enable SteamOS session on login manager..."
+sudo tee /usr/share/wayland-sessions/steam.desktop <<'EOF'
+[Desktop Entry]
+Encoding=UTF-8
+Name=Steam (GameScope)
+Comment=SteamOS session using GameScope compositor
+Exec=gamescope-session-plus steam
+Type=Application
+DesktopNames=steamos
+EOF
 
 sudo systemctl enable sddm.service
