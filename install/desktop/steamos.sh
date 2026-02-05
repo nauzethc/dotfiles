7c8787e01da70e6ac53f184ac9762454008d6009
Nauzet Hernandez <nauzethc@gmail.com>
2026-02-05 13:58:18 +0000

    install Proton versions with flatpak

--- install/desktop/steamos.sh
+++ install/desktop/steamos.sh
@@ -8,23 +8,24 @@ sudo pacman -Sy --needed --noconfirm \
   steam
 
 # Install Wayland's HDR compatible Proton and Vulkan layer
-yay -S --needed --noconfirm \
+yay -S --noconfirm \
   gamescope-session-git \
   moondeckbuddy-appimage \
-  proton-ge-custom-bin \
   sunshine \
   vk-hdr-layer-kwin6-git
 
-sudo mkdir -p /usr/share/wayland-sessions
-sudo mkdir -p /usr/share/gamescope-session-plus/sessions.d
+# Install ProtonPlus to install ProtonGE or CachyOS versions
+flatpak install --noninteractive com.vysp3r.ProtonPlus
 
 # Add Steam session to Gamescope
+sudo mkdir -p /usr/share/gamescope-session-plus/sessions.d
 sudo tee /usr/share/gamescope-session-plus/sessions.d/steam <<'EOF'
 CLIENTCMD="steam -steamdeck -steamos3 -pipewire-dmabuf"
 EOF
 
 # Add SteamOS with GameScope session on login managers
 echo "Enable SteamOS session on login manager..."
+sudo mkdir -p /usr/share/wayland-sessions
 sudo tee /usr/share/wayland-sessions/steam.desktop <<'EOF'
 [Desktop Entry]
 Encoding=UTF-8
