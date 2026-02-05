a3120c1a8fb79c23b74e4e509f7f1d195770be11
Nauzet Hernandez <nauzethc@gmail.com>
2026-02-05 13:59:03 +0000

    added kernel driver to blacklist

--- install/hardware/xbox.sh
+++ install/hardware/xbox.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # Install new Xbox driver compatible with Xbox Elite Controller 2
-yay -Sy --needed --noconfirm xpadneo-dkms
+sudo pacman -S --noconfirm --needed linux-headers
+yay -S --noconfirm xpadneo-dkms
 
-# Give this user privileged input access for dictation tools + xbox controllers to work
-sudo usermod -aG input ${USER}
+# Prevent xpad/xpadneo driver conflict
+echo blacklist xpad | sudo tee /etc/modprobe.d/blacklist-xpad.conf >/dev/null
+echo hid_xpadneo | sudo tee /etc/modules-load.d/xpadneo.conf >/dev/null
+
+# Give user access to game controllers
+sudo usermod -a -G input $USER
